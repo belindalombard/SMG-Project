368d12eca441c1ca74b8605e86714e3db340584b
Linda <linda.zungu@icloud.com>
2020-08-30 16:17:51 +0200

    removed one search button

--- homeView.java
+++ homeView.java
@@ -26,43 +26,43 @@ public class homeView{
 	
 	
 	
-	flowPanel = new JPanel(new FlowLayout());
+	    flowPanel = new JPanel(new FlowLayout());
         bPanel = new JPanel(new BorderLayout());
 
         //search field
         searchField = new JTextField(20);
 
-        search = new JButton("Search");
-	search.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			//bPanel.add(searchLabel);
-			searchLabel.setPreferredSize(new Dimension(600,400));
-			searchLabel.setBorder(plainborder);
-			searchLabel.setFont(font);
-			searchLabel.setText("Search Results Will Appear Here");
-			searchLabel.setHorizontalAlignment(JLabel.CENTER);				
-		}
-	});
+        searchButton = new JButton("Search");
+        searchButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //bPanel.add(searchLabel);
+                searchLabel.setPreferredSize(new Dimension(600,400));
+                searchLabel.setBorder(plainborder);
+                searchLabel.setFont(font);
+                searchLabel.setText("Search Results Will Appear Here");
+                searchLabel.setHorizontalAlignment(JLabel.CENTER);
+            }
+        });
 
-	//set check boxes for filters
-	localbased = new Checkbox("Local-based", true);
-	countrybased = new Checkbox("Country-based");
-	shop = new Checkbox("Shop", true);
-	products = new Checkbox("Filters");
-	filters = new JPanel(new FlowLayout());
-	filters.add(localbased);
-	filters.add(countrybased);
-	filters.add(shop);
-	filters.add(products);	
+        //set check boxes for filters
+        localbased = new Checkbox("Local-based", true);
+        countrybased = new Checkbox("Country-based");
+        shop = new Checkbox("Shop", true);
+        products = new Checkbox("Filters");
+        filters = new JPanel(new FlowLayout());
+        filters.add(localbased);
+        filters.add(countrybased);
+        filters.add(shop);
+        filters.add(products);
 
 
         //search button
-        searchButton = new JButton("Search");
+//        searchButton = new JButton("Search");
         //
 
 
         //the next button
-        nextViewButton = new JButton("Next -> Shop");
+        nextViewButton = new JButton("Shop");
         nextViewButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -72,10 +72,9 @@ public class homeView{
         });
 
 
-
-	center_layout = new JPanel();
+        center_layout = new JPanel();
         flowPanel.add(searchField);
-        flowPanel.add(search);
+//        flowPanel.add(search);
 
         flowPanel.add(searchField);
         flowPanel.add(searchButton);
@@ -83,30 +82,23 @@ public class homeView{
 
         bPanel.add(nextViewButton, BorderLayout.SOUTH);
         bPanel.add(flowPanel, BorderLayout.NORTH);
-	center_layout.add(filters);
-	center_layout.add(searchLabel);
-	bPanel.add(center_layout, BorderLayout.CENTER);
+        center_layout.add(filters);
+        center_layout.add(searchLabel);
+        bPanel.add(center_layout, BorderLayout.CENTER);
 
-	//Set Background Colors
-	flowPanel.setBackground(new Color(118,215,196));
-	window.setBackground(new Color(118,215,196));
-	center_layout.setBackground(new Color(118,215,196));
-	searchLabel.setBackground(new Color(209,242,235));
-	filters.setBackground(new Color(119,215,196));
+        //Set Background Colors
+        flowPanel.setBackground(new Color(118,215,196));
+        window.setBackground(new Color(118,215,196));
+        center_layout.setBackground(new Color(118,215,196));
+        searchLabel.setBackground(new Color(209,242,235));
+        filters.setBackground(new Color(119,215,196));
 
 
-	window.add(bPanel);
-	window.setVisible(true);
-  
+        window.add(bPanel);
+        window.setVisible(true);
 
-  
     }
 
-
-
-    public static void main(String [] args){
-        homeView x = new homeView();
-    }
 }
 
 

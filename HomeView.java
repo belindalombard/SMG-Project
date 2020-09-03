8cb04cf5da7e078cac79ef0d1a8ba4a416be9c65
Linda <linda.zungu@icloud.com>
2020-09-03 23:38:48 +0200

    HomeView added

---
+++ HomeView.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class HomeView {
+    public HomeView(){
+        //Frame
+        JFrame window = new JFrame("Sell My Goods: Login");
+        window.setMinimumSize(new Dimension(800, 600));
+        window.setLocation(450, 200);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //
+
+
+
+        window.setVisible(true);
+    }
+}

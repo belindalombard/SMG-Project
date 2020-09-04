93f90a2d0a4309c35098083099ef681381bf7a8b
Linda <linda.zungu@icloud.com>
2020-09-04 23:24:13 +0200

    List of shops added in the homeview

--- HomeView.java
+++ HomeView.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeView extends SignUpView{
     public HomeView(JFrame previousWindowFrame){
@@ -13,8 +17,29 @@ public class HomeView extends SignUpView{
         window.setResizable(false);
         //
 
-        
+        JPanel topSearchBarLayout = new JPanel(new FlowLayout());
+        JTextField searchBar = new JTextField(25);
+        JButton searchButton = new JButton("Search");
 
+        String [] shops = new String[100];
+        for(int i = 0; i < 100; i++){
+            shops[i] = "Shop "+i;
+        }
+        JList shopList = new JList(shops);
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setViewportView(shopList);
+        shopList.setLayoutOrientation(JList.VERTICAL);
+        shopList.setFixedCellHeight(60);
+        shopList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+            }
+        });
+
+        topSearchBarLayout.add(searchBar);
+        topSearchBarLayout.add(searchButton);
+        window.add(topSearchBarLayout, BorderLayout.NORTH);
+        window.add(scrollPane);
         window.setVisible(true);
     }
 }

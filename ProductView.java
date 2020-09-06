2b020c23b00af32899d744acc8dc4284d4ddf9cf
Linda <linda.zungu@icloud.com>
2020-09-06 02:53:38 +0200

    Quantity entry now works perfectly

--- ProductView.java
+++ ProductView.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class ProductView {
 
     int quantityOfItems = 1;
+    int availableStockItems = 160;
 
     public ProductView(JFrame previousWindowFrame){
         //Frame
@@ -31,54 +32,102 @@ public class ProductView {
             }
         });
 
+
         JLabel productImage = new JLabel(new ImageIcon("/Users/lindazungu/Desktop/Sell My Goods/src/sampleImage.png"));
         productImage.setBounds(80, 100, 250, 250);
 
         JLabel productName = new JLabel();
         productName.setText("Product Name");
         productName.setFont(new Font(null, Font.BOLD, 20));
-        productName.setBounds(350, 100, 150, 50);
+        productName.setBounds(400, 100, 150, 50);
 
         JLabel productPrice = new JLabel();
         productPrice.setText("Product Price : R270.00");
-        productPrice.setBounds(350, 150, 150, 50);
+        productPrice.setBounds(400, 150, 150, 50);
 
         JTextArea productDetailsLabel = new JTextArea();
         productDetailsLabel.setText("Product Details :\nHello World, this product is so \ncool, like really cool! \nIt does everything");
-        productDetailsLabel.setBounds(350, 200, 400, 85);
+        productDetailsLabel.setBounds(400, 200, 350, 85);
         productDetailsLabel.setBackground(window.getBackground());
 
-        JLabel availableStock = new JLabel("Available Stock : 10");
-        availableStock.setBounds(350, 270, 150, 50);
+        JLabel availableStock = new JLabel("Available Stock : "+availableStockItems);
+        availableStock.setBounds(400, 270, 150, 50);
 
         JTextField quantityField = new JTextField(10);
-        quantityField.setBounds(390, 320, 70, 27);
-//        quantityField.setText(""+quantityOfItems);
+        quantityField.setBounds(440, 320, 70, 27);
         quantityField.setText("1");
         quantityField.setHorizontalAlignment(SwingConstants.CENTER);
 
         JButton addItemButton = new JButton("+");
-        addItemButton.setBounds(340, 321, 50, 25);
+        addItemButton.setBounds(390, 321, 50, 25);
         addItemButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                quantityOfItems = Integer.parseInt(quantityField.getText())+1;
-                quantityField.setText(""+quantityOfItems);
+                if(quantityField.getText().isEmpty()){
+//                    JOptionPane.showMessageDialog(null, "Error: Invalid quantity of items");
+                    quantityOfItems = 1;
+                    quantityField.setText("" + quantityOfItems);
+                }
+
+                else if(Integer.parseInt(quantityField.getText()) < availableStockItems){
+                    quantityOfItems = Integer.parseInt(quantityField.getText())+1;
+                    quantityField.setText(""+quantityOfItems);
+
+                }
+                else{
+                    quantityOfItems = availableStockItems;
+                    quantityField.setText(""+quantityOfItems);
+                }
             }
         });
 
         JButton subtractItemButton = new JButton("-");
-        subtractItemButton.setBounds(460, 321, 50, 25);
+        subtractItemButton.setBounds(510, 321, 50, 25);
         subtractItemButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                quantityOfItems = Integer.parseInt(quantityField.getText())-1;
-                quantityField.setText(""+quantityOfItems);
+                if(quantityField.getText().isEmpty()){
+//                    JOptionPane.showMessageDialog(null, "Error: Invalid quantity of items");
+                    quantityOfItems = 1;
+                    quantityField.setText("" + quantityOfItems);
+                }
+
+                else if (Integer.parseInt(quantityField.getText()) > availableStockItems) {
+                    quantityOfItems = availableStockItems;
+                    quantityField.setText("" + quantityOfItems);
+                }
+
+                else if (Integer.parseInt(quantityField.getText()) > 1) {
+                    quantityOfItems = Integer.parseInt(quantityField.getText()) - 1;
+                    quantityField.setText("" + quantityOfItems);
+                }
             }
         });
 
+        JButton payButton = new JButton("Pay");
+        payButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(quantityField.getText().isEmpty()){
+                    JOptionPane.showMessageDialog(null, "Error: Invalid quantity of items");
+                }
+                else if(Integer.parseInt(quantityField.getText()) < 1){
+                    JOptionPane.showMessageDialog(null, "Entry unacceptable");
+                }
+                else if(Integer.parseInt(quantityField.getText()) > availableStockItems){
+                    JOptionPane.showMessageDialog(null, "Too many more than available stock items requested");
+                }
+                else if(Integer.parseInt(quantityField.getText()) <= availableStockItems){
+                    PaymentView paymentView = new PaymentView(window);
+                    window.setVisible(false);
+                }
+
+            }
+        });
 
         topButtonLayout.add(backButton);
+        topButtonLayout.add(Box.createHorizontalGlue());
+        topButtonLayout.add(payButton);
 
         window.add(topButtonLayout, BorderLayout.NORTH);
         window.add(productName);
@@ -93,8 +142,4 @@ public class ProductView {
         window.add(new JLabel());
         window.setVisible(true);
     }
-
-//    public static void main(String [] args){
-//        ProductView x = new ProductView(new JFrame());
-//    }
 }

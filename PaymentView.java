bf57c90620470710f9fd89c4cd16baf5e0005e30
Linda <linda.zungu@icloud.com>
2020-09-29 15:02:42 +0200

    Now is a pop-up view instead of a standalone view.

--- PaymentView.java
+++ PaymentView.java
@@ -4,11 +4,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PaymentView {
-    public PaymentView(JFrame previousWindowFrame){
+    public PaymentView(JFrame previousWindowFrame, JButton payButton, JButton preBackButton, JTextField quantityField, JButton addButton, JButton subtractButton, JButton chatButton){
         //Frame
         JFrame window = new JFrame("Sell My Goods: Payment");
-        window.setMinimumSize(new Dimension(800, 600));
-        window.setLocation(300, 150);
+        window.setMinimumSize(new Dimension(400, 400));
+        window.setLocation(600, 150);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         //
@@ -21,6 +21,12 @@ public class PaymentView {
             public void actionPerformed(ActionEvent e) {
                 previousWindowFrame.setVisible(true);
                 window.setVisible(false);
+                payButton.setEnabled(true);
+                preBackButton.setEnabled(true);
+                quantityField.setEditable(true);
+                addButton.setEnabled(true);
+                subtractButton.setEnabled(true);
+                chatButton.setEnabled(true);
             }
         });
 
@@ -30,13 +36,19 @@ public class PaymentView {
             public void actionPerformed(ActionEvent e) {
                 ConfirmationView confirmationView = new ConfirmationView();
                 window.setVisible(false);
+                previousWindowFrame.setVisible(false);
             }
         });
+
+//        JLabel itemBoughtLabel = new JLabel("Item : ");
+//        itemBoughtLabel.setBounds(150, 100, 150, 27);
 
         topButtonLayout.add(backButton);
         topButtonLayout.add(Box.createHorizontalGlue());
         topButtonLayout.add(paymentButton);
 
+//        window.add(itemBoughtLabel);
+        window.add(new JLabel());
         window.add(topButtonLayout, BorderLayout.NORTH);
         window.setVisible(true);
     }

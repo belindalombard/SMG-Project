66dc6d302452a172812b9459b12374751ca79b5b
Linda <linda.zungu@icloud.com>
2020-10-01 21:31:48 +0200

    Confirmation of payment shows a summary of what the user is about to pay for.

--- PaymentView.java
+++ PaymentView.java
@@ -4,11 +4,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PaymentView {
-    public PaymentView(JFrame previousWindowFrame, JButton payButton, JButton preBackButton, JTextField quantityField, JButton addButton, JButton subtractButton, JButton chatButton){
+    public PaymentView(JFrame previousWindowFrame, JButton payButton, JButton preBackButton, JTextField quantityField, JButton addButton, JButton subtractButton, JButton chatButton, String productName, JLabel totalAmount){
         //Frame
         JFrame window = new JFrame("Sell My Goods: Payment");
         window.setMinimumSize(new Dimension(400, 400));
-        window.setLocation(600, 150);
+        window.setLocation(500, 150);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         //
@@ -30,7 +30,7 @@ public class PaymentView {
             }
         });
 
-        JButton paymentButton = new JButton("Confirm Payment");
+        JButton paymentButton = new JButton("Confirm");
         paymentButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -40,14 +40,22 @@ public class PaymentView {
             }
         });
 
-//        JLabel itemBoughtLabel = new JLabel("Item : ");
-//        itemBoughtLabel.setBounds(150, 100, 150, 27);
+        JLabel itemBoughtLabel = new JLabel("Item : "+productName);
+        itemBoughtLabel.setBounds(150, 100, 150, 27);
+
+        JLabel quantityBoughtLabel = new JLabel("Number of Items : "+quantityField.getText());
+        quantityBoughtLabel.setBounds(150, 150, 150, 27);
+
+        JLabel totalAmountSpentLabel = new JLabel(totalAmount.getText());
+        totalAmountSpentLabel.setBounds(150, 200, 150, 27);
 
         topButtonLayout.add(backButton);
         topButtonLayout.add(Box.createHorizontalGlue());
         topButtonLayout.add(paymentButton);
 
-//        window.add(itemBoughtLabel);
+        window.add(itemBoughtLabel);
+        window.add(quantityBoughtLabel);
+        window.add(totalAmountSpentLabel);
         window.add(new JLabel());
         window.add(topButtonLayout, BorderLayout.NORTH);
         window.setVisible(true);
